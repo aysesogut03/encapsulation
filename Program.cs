93a86e604c210b595e7e1ae439d7437d5e7bd639
aysesogut03 <aysesogut2003@gmail.com>
2024-12-11 10:43:26 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ogrenci ogrenci1= new ogrenci();
+            ogrenci1.setisim("enes");
+            string gelenisim= ogrenci1.getisim();
+            if (gelenisim == "enes")
+            {
+                Console.WriteLine("Başarılı");
+            }
+            else
+            {
+                Console.WriteLine("başarısız");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ogrenci.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    public class ogrenci
+    {
+        private string isim;
+        public void setisim(string _isim)
+        {
+            isim = _isim;
+        }
+        
+        public string getisim() 
+        {
+            return isim;
+        }
+    }
+}
